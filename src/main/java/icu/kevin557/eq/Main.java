7b1c590bfabc32f4a706e9ab29e9e2351fc44d25
kkw557 <kevin557557@gmail.com>
2022-11-17 10:07:55 +0800

    init

---
+++ src/main/java/icu/kevin557/eq/Main.java
@@ -0,0 +1,10 @@
+package icu.kevin557.eq;
+
+import net.mamoe.mirai.Bot;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Bot.getInstances().size());
+    }
+}
